b9703f71c7d8cc7dea21348bf01028ba0e104ab7
dorianbonnet <bonnetdorian9551@gmail.com>
2023-06-13 08:38:35 +0200

    update

--- index.js
+++ index.js
@@ -104,6 +104,6 @@ app.listen(3000, () => {
 // });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('API server is running on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('API server is running on port 3000');
+// });
