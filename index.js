b901d6f57536fb1feedca71e1e889b36d3432da4
dorianbonnet <bonnetdorian9551@gmail.com>
2023-06-14 08:57:21 +0200

    update

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/articles', (req, res) => {
       res.status(500).json({ error: 'Failed to read articles' });
     } else {
     const articles = files.map((file, index) => {
+        console.log(file);
         const filePath = path.join(articlesDir, file);
         const articleContent = fs.readFileSync(filePath, 'utf-8');
         return { id: `article${index}`, filename: file, content: articleContent };
@@ -29,6 +30,7 @@ app.get('/articles', (req, res) => {
 // Define an API endpoint to serve individual articles
 app.get('/articles/:filename', (req, res) => {
   const { filename } = req.params;
+  console.log(req.params);
   const articlesDir = path.join(__dirname, 'articles');
   const filePath = path.join(articlesDir, filename);
   
