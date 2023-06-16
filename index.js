593f3b3abca474cc958118853c940227042e2ea9
dorianbonnet <bonnetdorian9551@gmail.com>
2023-06-16 12:45:56 +0200

    update

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get('/articles', (req, res) => {
 // Define an API endpoint to serve individual articles
 app.get('/articles/:filename', (req, res) => {
   const { filename } = req.params;
-  console.log(req.params);
   const articlesDir = path.join(__dirname, 'articles');
   const filePath = path.join(articlesDir, filename);
   
